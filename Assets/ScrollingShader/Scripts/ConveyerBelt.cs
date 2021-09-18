d93523ad1d1ef381ef5799cf664fe5201c4dc67a
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-09-17 20:20:22 -0700

    Belt Movement Adjustment (#7)
    * Attempted to add new pull model for belt mvoement
    
    * Updated belts to support new belt movement mode
    
    * updated scenes and prefabs based on current results

--- Assets/ScrollingShader/Scripts/ConveyerBelt.cs
+++ Assets/ScrollingShader/Scripts/ConveyerBelt.cs
@@ -3,34 +3,101 @@ using UnityEngine;
 namespace nickmaltbie.ScrollingShader
 {
     /// <summary>
-    /// Relative direction to face from a local transform.
-    /// </summary>
-    public enum RelativeDirection
-    {
-        Up,
-        Down,
-        Left,
-        Right,
-        Forward,
-        Backward
-    }
-
-    /// <summary>
     /// Conveyer belt that has a uniform direction and speed and will push objects that collide with it.
     /// </summary>
     public class ConveyerBelt : MonoBehaviour
     {
         /// <summary>
+        /// How does this belt move objects, by pushing objects sitting
+        /// on top of the belt or by "moving" the belt forward and pulling
+        /// them forward with the object.
+        /// </summary>
+        public enum BeltForceMode
+        {
+            Push,
+            Pull
+        }
+
+        /// <summary>
+        /// Relative direction to face from a local transform.
+        /// </summary>
+        public enum RelativeDirection
+        {
+            Up,
+            Down,
+            Left,
+            Right,
+            Forward,
+            Backward
+        }
+
+        /// <summary>
         /// Velocity of the conveyer belt in meters per second.
         /// </summary>
         [SerializeField]
+        [Tooltip("Velocity of the conveyer belt.")]
         private float velocity;
 
         /// <summary>
         /// Direction that the conveyer belt pushes objects.
         /// </summary>
         [SerializeField]
+        [Tooltip("Local direction does this push objects.")]
         private RelativeDirection direction = RelativeDirection.Down;
+
+        /// <summary>
+        /// Kind of force used to move objects. <see cerf="BeltForceMode" /> for more details on 
+        /// how the belt force modes work.
+        /// </summary>
+        // [SerializeField]
+        // [Tooltip("Belt force mode used to push physics objects in the scene.")]
+        private BeltForceMode beltMode = BeltForceMode.Push;
+
+        /// <summary>
+        /// Rigidbody associated with conveyer belt.
+        /// </summary>
+        private Rigidbody body;
+
+        /// <summary>
+        /// Position of the conveyer belt.
+        /// </summary>
+        private Vector3 pos;
+
+        /// <summary>
+        /// Awake event to initialize belt configuration.
+        /// </summary>
+        public void Awake()
+        {
+            this.body = GetComponent<Rigidbody>();
+            pos = transform.position;
+        }
+
+        /// <summary>
+        /// Fixed update to move the belt and pull objects along with it.
+        /// </summary>
+        public void FixedUpdate()
+        {
+            if (body != null && beltMode == BeltForceMode.Pull)
+            {
+                Vector3 movement = velocity * GetDirection() * Time.fixedDeltaTime;
+                transform.position = pos - movement;
+                body.MovePosition(pos);
+            }
+        }
+
+        /// <summary>
+        /// When colliding with an object, if it has a rigidbody and is not kinematic, push it forward by the speed of
+        /// the conveyer belt.
+        /// </summary>
+        /// <param name="other">Collision event between the objects.</param>
+        public void OnCollisionStay(Collision other)
+        {
+            if (other.rigidbody != null && !other.rigidbody.isKinematic && beltMode == BeltForceMode.Push)
+            {
+                Vector3 movement = velocity * GetDirection() * Time.deltaTime;
+                other.rigidbody.MovePosition(other.transform.position + movement);
+            }
+        }
 
         /// <summary>
         /// Get the world space direction that the conveyer belt will push objects.
@@ -54,20 +121,6 @@ namespace nickmaltbie.ScrollingShader
                     return -transform.forward;
             }
             return transform.forward;
-        }
-
-        /// <summary>
-        /// When colliding with an object, if it has a rigidbody and is not kinematic, push it forward by the speed of
-        /// the conveyer belt.
-        /// </summary>
-        /// <param name="other">Collision event between the objects.</param>
-        public void OnCollisionStay(Collision other)
-        {
-            if (other.rigidbody != null && !other.rigidbody.isKinematic)
-            {
-                Vector3 movement = velocity * GetDirection() * Time.deltaTime;
-                other.rigidbody.MovePosition(other.transform.position + movement);
-            }
         }
     }
 }
